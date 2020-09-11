074cc730470d3c6ab6f1c6a3069669de3b71e48b
Fabian Quoidbach <fabianquoidbach@gmail.com>
2020-09-11 16:56:15 +0200

    Début timer

--- src/components/controls/startstop.jsx
+++ src/components/controls/startstop.jsx
@@ -1,20 +1,9 @@
-import React, {useEffect} from "react";
+import React from "react";
 
-const StartStop = ({seconds, setSeconds, isActive, setIsActive}) => {
+const StartStop = ({isActive, setIsActive}) => {
     const toggle = () => {
         setIsActive(!isActive);
     };
-
-    useEffect(() => {
-        let interval = null;
-        if (isActive) {
-            interval = setTimeout(() => {
-                setSeconds(seconds - 1);
-            }, 1000);
-        } else {
-            clearTimeout(interval);
-        }
-    });
 
     return (
         <button className={"startStop"} onClick={toggle}>
--- src/components/timer/timer.jsx
+++ src/components/timer/timer.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import AddTime from "../controls/addtime";
 import MinusTime from "../controls/minustime";
 import StartStop from "../controls/startstop";
@@ -8,6 +8,17 @@ const Timer = () => {
     const [minutes, setMinutes] = useState(10);
     const [seconds, setSeconds] = useState(15);
     const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+        let interval = null;
+        if (isActive) {
+            interval = setTimeout(() => {
+                setSeconds(seconds - 1);
+            }, 1000);
+        } else {
+            clearTimeout(interval);
+        }
+    });
 
     return (
         <div>
@@ -20,14 +31,7 @@ const Timer = () => {
                 {seconds}
             </h1>
             <MinusTime minutes={minutes} setMinutes={setMinutes} />
-            <StartStop
-                minutes={minutes}
-                setMinutes={setMinutes}
-                seconds={seconds}
-                setSeconds={setSeconds}
-                isActive={isActive}
-                setIsActive={setIsActive}
-            />
+            <StartStop isActive={isActive} setIsActive={setIsActive} />
         </div>
     );
 };
